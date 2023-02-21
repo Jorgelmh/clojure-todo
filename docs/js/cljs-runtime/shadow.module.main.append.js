5515be20207602ab5419b6ed237cfb10f41f79ee
Jorge <jorgelmh_01@hotmail.com>
2023-02-21 00:14:50 +0000

    Fix public folder

--- docs/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { my_project.app.core.main(); } catch (e) { console.error("An error occurred when calling (my-project.app.core/main)"); throw(e); }
